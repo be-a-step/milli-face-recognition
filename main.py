9006b30a72ed029313c57954b596c5a6af1ac18e
lwnica <nanami.nica@gmail.com>
2019-04-23 03:14:28 +0900

    fix file path

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import sampling_image
 import triming_face
 
-cascade_conf_path = "./resources/lbpcascade_animeface.xml"
-source_directory = './resources/source/movies'
-images_dist = './resources/source/images'
-sampling_rate = 1000
+cascade_conf_path = "./resources/conf/cv2-cascade-conf/lbpcascade_animeface.xml"
+source_directory = './resources/sources/movies'
+images_dist = './resources/sources/images'
+sampling_rate = 10
 margin = (20, 20, 20, 0)
 
 sampling_image.sampling_from_movies(
     source_directory, images_dist, sampling_rate)
 
-traning_dist = './resources/training/faces'
+traning_dist = './resources/dataset/faces'
 
 triming_face.triming_images(
     images_dist, traning_dist, margin, cascade_conf_path)
